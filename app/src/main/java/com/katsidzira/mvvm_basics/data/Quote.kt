ac018c2a0a8d29ca2828720be85374e7810455c0
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MacBook-Pro-2.local>
2020-03-22 21:59:41 -0400

    created fake dao class

--- app/src/main/java/com/katsidzira/mvvm_basics/data/Quote.kt
+++ app/src/main/java/com/katsidzira/mvvm_basics/data/Quote.kt
@@ -1,4 +1,8 @@
 package com.katsidzira.mvvm_basics.data
 
 data class Quote(val text: String, val author: String) {
+
+    override fun toString(): String {
+        return "$text - $author"
+    }
 }
